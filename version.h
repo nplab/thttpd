7a4db6fe9d0e840e417ed000b189f4f3cc40c10e
Michael Tuexen <tuexen@fh-muenster.de>
2015-11-20 19:42:33 +0100

    Identify SCTP support in name.

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define SERVER_SOFTWARE "thttpd/2.27 19Oct2015"
-#define SERVER_ADDRESS "http://www.acme.com/software/thttpd/"
+#define SERVER_SOFTWARE "thttpd-sctp/2.27 20Nov2015"
+#define SERVER_ADDRESS "https://github.com/nplab/thttpd/"
 
 #endif /* _VERSION_H_ */
